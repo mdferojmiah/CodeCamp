3180b06894368feb95814ca9a2401e6a1f4e6974
Feroj Miah <01ferojmiah@gmail.com>
2026-08-19 01:24:12 +0600

    [ServerCamp] encryption decryption modified using IDataProtectionProvider

--- dotnet/Session21/learning-identity/Program.cs
+++ dotnet/Session21/learning-identity/Program.cs
@@ -6,13 +6,20 @@ using System.Text;
 using learning_identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection();
 
 var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
+    var provider = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
+    var protector = provider.CreateProtector("Cookie.Protector");
+
     var authCookie = context.Request.Headers.Cookie
                         .FirstOrDefault(c => c!.StartsWith("codecamp"));
 
@@ -23,15 +30,16 @@ app.Use(async (context, next) =>
     }
 
     var encryptedCookie = authCookie?.Split("=").Last();
-    var encryptedParts = encryptedCookie?.Split("-");
-    if(encryptedParts == null || encryptedParts.Length < 2)
-    {
-        await next();
-        return;
-    }
-    var encrptedSecret = encryptedParts?[0];
-    var initializedVector = encryptedParts?[1];
-    var payload = EncryptionManager.Decrypt(encrptedSecret!, initializedVector!);
+    // var encryptedParts = encryptedCookie?.Split("-");
+    // if(encryptedParts == null || encryptedParts.Length < 2)
+    // {
+    //     await next();
+    //     return;
+    // }
+    // var encrptedSecret = encryptedParts?[0];
+    // var initializedVector = encryptedParts?[1];
+    // var payload = EncryptionManager.Decrypt(encrptedSecret!, initializedVector!);
+    var payload = protector.Unprotect(encryptedCookie!);
     var parts = payload?.Split(":");
     var key = parts?[0];
     var value = parts?[1];
@@ -62,15 +70,19 @@ app.MapGet("/cookie-authorized", (HttpContext context) =>
     return Results.Ok("user is cookie authorized!");
 });
 
-app.MapGet("/login", (string username, string password, HttpContext context) =>
+app.MapGet("/login", (string username, string password, HttpContext context, IDataProtectionProvider provider) =>
 {
+
     if(username != "feroj" || password != "password") return Results.Unauthorized();
 
     var secret = $"secret:123456789";
 
-    var (encrptedSecret, initializedVector) = EncryptionManager.Encrypt(secret);
+    //var (encrptedSecret, initializedVector) = EncryptionManager.Encrypt(secret);
+    var protector = provider.CreateProtector("Cookie.Protector");
+    var encrypt = protector.Protect(secret);
+    context.Response.Headers["set-cookie"] = $"codecamp={encrypt}";
 
-    context.Response.Headers["set-cookie"] = $"codecamp={encrptedSecret}-{initializedVector}";
+    //context.Response.Headers["set-cookie"] = $"codecamp={encrptedSecret}-{initializedVector}";
 
     return Results.Ok("Login successful");
 });
